83f42a17686e0b57b9e3bd2640f8af41358a8d84
Jude Gibbons <judes@gimaju.net>
2015-07-28 23:01:51 +0100

    Third commit: improving styling and adding pagination; fixing errors in this and in the way the input was submitted.

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
 #Recipe finder
 
-Queries the [Guardian content API](http://open-platform.theguardian.com/access/) for recipe articles with certain keywords.
+The form queries the [Guardian content API](http://open-platform.theguardian.com/access/) 
+for recipe articles with certain keywords. The word 'recipe' is always included 
+by default: other words can be included or excluded.
 
-Currently able to include or exclude words. The word 'recipe' is always included.
+Certain defaults have been chosen and hard-coded into the url string: the search 
+is restricted to the Life and Style section, the number of results per page is set 
+to 20, and the sort order is newest article first.
+
+Still to do: 
+
+* add a couple of mediaquery rules to lay the page out better on mobile;
+
+* look into local storage in order to persist the page data when opening a new
+page in the same tab (although I am currently opening the retrieved links 
+in a new tab);
+
+* check submitted strings for extraneous characters (although I think the API 
+looks after the encoding);
+
+* add a loading spinner for when the page takes a while to retrieve results;
+
+* create radio buttons to allow the search fields to be set to search on 'all 
+words' (which is what it is currently), 'any word' or 'exact phrase';
+
+* add a logo and a default thumbnail image for when the article doesn't
+provide one (I am currently using a placeholder image).
--- js/recipe-finder.js
+++ js/recipe-finder.js
@@ -1,8 +1,14 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
+var recipeFinderModule = recipeFinderModule || {}; 
+
+recipeFinderModule.loadRecipes = function () {
 
 	var recipeFinder = document.getElementById("recipeFinder");
+	var apiKey = "ytwwwrbzgm48mn794m62tert";
+	var currentPageNumber;
+	var queryString = "";
 
-	function addListenerToRecipeFinder() { //try listener on form, event = 'submit'
+	document.addEventListener("DOMContentLoaded", function(event) { 
+
 		if (document.getElementById("submitButton").addEventListener) {
 			(document.getElementById("submitButton")).addEventListener('click', setParameters, false);
 		} else {
@@ -10,72 +16,168 @@ document.addEventListener("DOMContentLoaded", function(event) {
 			(document.getElementById("submitButton")).attachEvent('onclick', setParameters);
 			};
 		};
+	});
+
+	function setParameters() {
+
+		currentPageNumber = 1;
+
+		var regex = / /g;
+
+		var includeString = (document.getElementById("include").value).replace(/ /g, "%20AND%20");
+
+		if (includeString == "") {
+			includeString = "recipe";
+		} else {
+			includeString = "recipe%20AND%20".concat(includeString);
+		};
+
+		var excludeString = (document.getElementById("exclude").value).replace(/ /g, "%20AND%20NOT%20");
+
+		if (excludeString) {
+			queryString = includeString + "%20AND%20NOT%20" + excludeString;
+		} else {
+			queryString = includeString;
+		};
+
+        (event.preventDefault) ? event.preventDefault() : event.returnValue = false;
+
+		submitParameters(queryString);
+
 	};
-	addListenerToRecipeFinder();
-});
 
-function setParameters() {
 
-	var apiKey = "ytwwwrbzgm48mn794m62tert";
-	var queryString = "";
-	var includeString = (document.getElementById("include").value).replace(' ', "%20AND%20");//what if someone's typed in 2 spaces?
-	//need to take account of double spaces, other chars?
 
-	if (includeString == "") {
-		includeString = "recipe";
-	} else {
-		includeString = "recipe%20AND%20".concat(includeString);
+	function submitParameters(queryString) {
+
+		var urlWithParameters = "http://content.guardianapis.com/search?section=lifeandstyle" 
+		+ "&order-by=newest&show-fields=headline%2Cthumbnail%2CtrailText%2CfirstPublicationDate&page=" 
+		+ currentPageNumber + "&page-size=20&q=" + queryString + "&api-key=" + apiKey;
+
+		var xmlhttp = window.XMLHttpRequest ?
+	        new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+	    var searchResults;
+
+	    xmlhttp.onreadystatechange = function() {
+	        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+	        	searchResults = xmlhttp.responseText;
+	            var jsonParse = JSON.parse(searchResults);
+	        	displayResults(jsonParse);
+	        	displayPagination(jsonParse);
+	    	};
+	    };
+	    xmlhttp.open("GET", urlWithParameters, true);
+	    xmlhttp.send();
+
 	};
 
-	var excludeString = (document.getElementById("exclude").value).replace(' ', "%20AND%20NOT%20");
 
-	//do I need to use encodeURIComponent AFTER spaces removed? or does api take care of it?
-	//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/encodeURIComponent
+	function displayResults(jsonParse) {
+		var resultsDiv = document.getElementById("results");
+		var resultsDisplay = "";
 
-	if (excludeString) {
-		queryString = includeString + "%20AND%20NOT%20" + excludeString;
-	} else {
-		queryString = includeString;
+		var pageSize = jsonParse.response.pageSize;
+		var totalResults = jsonParse.response.total;
+		var currentPageNumber = jsonParse.response.currentPage;
+		var numberOfPages = jsonParse.response.pages;
+		var displayedDate;
+		var maxi;
+
+		if (currentPageNumber == numberOfPages) {
+			maxi = Math.min(pageSize, totalResults%pageSize);			
+		} else {
+			maxi = pageSize;
+		};
+
+		for (i=0; i<maxi; i++) {
+			var webUrl = jsonParse.response.results[i].webUrl;
+			var thumbnail = jsonParse.response.results[i].fields.thumbnail || "./images/tempthumb.png";
+			var firstPublicationDate = jsonParse.response.results[i].fields.firstPublicationDate || "";
+			
+			if (firstPublicationDate != "") {
+				var dateArray = firstPublicationDate.split("T")[0].split("-") || "";
+				var monthString = dateArray[1];
+				var mapMonthString = {
+					"01":" January ", "02":" February ", "03":" March ", "04":" April ", "05":" May ", "06":" June ", "07":" July ", "08":" August ", "09":" September ", "10":" October ", "11":" November ", "12":" December "
+				};
+				monthString = monthString.replace(/01|02|03|04|05|06|07|08|09|10|11|12/g, function(matched) {
+					return mapMonthString[matched];
+				});
+				displayedDate = dateArray[2] + monthString + dateArray[0];
+
+			} else {
+				displayedDate = "";
+			};
+
+			var headline = jsonParse.response.results[i].fields.headline;
+			var trailText = jsonParse.response.results[i].fields.trailText;
+			var itemResult = "<li><span class='thumbnail'><img src='" + thumbnail + "' alt=' '></span>"
+			+ "<h2><a href='" + webUrl + "' target='_blank'>" + headline + "</a></h2>" 
+			+ "<p class='trailText'>" + trailText + "</p>"
+			+ "<p class='date'>" + displayedDate + "</p></li>"
+			resultsDisplay = resultsDisplay + itemResult;
+		};
+
+		resultsDiv.innerHTML = "<ul>" + resultsDisplay + "</ul>";
+
 	};
 
-	var urlWithParameters = "http://content.guardianapis.com/search?show-fields=all&q=" + queryString + "&api-key=" + apiKey;
-	
-	submitParameters(event,urlWithParameters);
+	function displayPagination(jsonParse) {
 
-};
+		var paginationDiv = document.getElementById("pagination");
+		var paginationDisplay = "";
+		paginationDiv.innerHTML = "<p class='resultsNumber'>Your search found " + jsonParse.response.total + " results</p>"
+								+ "<p class='pageCount'><span id='previousArrow'><a href='#test1'><span class='a11y-hide'>previous page</span><<</a></span>Page " + jsonParse.response.currentPage 
+								+ " of " + jsonParse.response.pages + "<span id='nextArrow'><a href='#test2'><span class='a11y-hide'>next page</span>>></a></span></p>";
 
-function submitParameters(event,urlWithParameters) {
+   		var nextLink = document.getElementById('nextArrow').firstElementChild;
+    	var previousLink = document.getElementById('previousArrow').firstElementChild;
 
-	(event.preventDefault) ? event.preventDefault() : event.returnValue = false;
-	var xmlhttp= window.XMLHttpRequest ?
-        new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
-    var searchResults;
+		function addListenerToPagination() {
+			if (paginationDiv.addEventListener) {
+				paginationDiv.addEventListener('click', loadNewPage, false);
+			} else {
+			if (paginationDiv.attachEvent) {
+				paginationDiv.attachEvent('onclick', loadNewPage);
+				};
+			};
 
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        	searchResults = xmlhttp.responseText; //NB i get a flash of 'undefined' - need to pause?
-        	displayResults(searchResults,urlWithParameters);
-    	};
+			removeClickableLinkOnPagination();
+		};
+
+        function removeClickableLinkOnPagination() {
+
+        	if (currentPageNumber == 1) {
+        		document.getElementById('previousArrow').className = "disabled";
+        		previousLink.firstElementChild.className = "a11y-disabled";
+        	}
+
+        	if (currentPageNumber == jsonParse.response.pages) {
+        		document.getElementById('nextArrow').className = "disabled";
+        		nextLink.firstElementChild.className = "a11y-disabled";
+        	}
+        };
+
+        addListenerToPagination();
+
     };
-    xmlhttp.open("GET", urlWithParameters, true);
-    xmlhttp.send();
+
+	function loadNewPage(e) {
+
+   		var nextLink = document.getElementById('nextArrow').firstElementChild;
+    	var previousLink = document.getElementById('previousArrow').firstElementChild;
+
+	    if (e.target == nextLink) {
+            currentPageNumber++;
+	    } else if (e.target == previousLink) {
+            currentPageNumber--;
+	    };
+	    
+	    e.preventDefault();
+
+	    submitParameters(queryString);
+
+    };
+	
 };
-
-function displayResults(searchResults,urlWithParameters) {
-	var resultsDiv = document.getElementById("results");
-	var resultsDisplay = "";
-	var jsonParse = JSON.parse(searchResults);
-
-	for (i=0, maxi=jsonParse.response.pageSize; i<maxi; i++) {
-		var thumbnail = jsonParse.response.results[i].fields.thumbnail;
-		var webUrl = jsonParse.response.results[i].webUrl;
-		var headline = jsonParse.response.results[i].fields.headline;
-		var trailText = jsonParse.response.results[i].fields.trailText;
-		var itemResult = "<li><span class='thumbnail'><img src='" + thumbnail + "' alt=' '></span><a href='" + webUrl + "'>" + headline + "</a><span class='trailText'>" + trailText + "</span></li>"
-		resultsDisplay = resultsDisplay + itemResult;
-	};
-
-	resultsDiv.innerHTML = "<ul>" + resultsDisplay + "</ul>";
-};
-
-//pagination: should display total no of results, 1-10, current page; webtitle <weburl>, date, 
+recipeFinderModule.loadRecipes();
